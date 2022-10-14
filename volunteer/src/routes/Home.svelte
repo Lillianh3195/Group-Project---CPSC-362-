a31d5c65ec1ae15640fbbebedeba592f69a82475
lori33c <lcha2@uci.edu>
2022-10-14 11:36:49 -0700

    navigation bar

--- volunteer/src/routes/Home.svelte
+++ volunteer/src/routes/Home.svelte
@@ -1,7 +1,22 @@
 <script>
-
+      import CreateProject from "./CreateProject.svelte";
+    var menu = 1;
 </script>
 
 <main>
-<p> Hello, This is the homepage</p>
+    <h1>Volunteer Website</h1>
+    <p>
+      Hello! This is our Volunteer Website.
+    </p>
+    <CreateProject/>
 </main>
+
+<style>
+    p {
+	    border-radius: 5px;
+		background-color: #e9e9e9;
+		padding: 10px;
+		margin: 10px;
+		width: 300px;
+	}
+</style>
